2e441c041a1344a814cbee8f18cf548570ec4b03
rns <renns@newhope.cn>
2017-03-27 19:18:02 +0800

    first commit

---
+++ src/main/java/com/hama/array/towSum/Solution.java
@@ -0,0 +1,27 @@
+package com.hama.array.towSum;
+
+/**
+ *
+ * Given an array of integers,
+ * return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution,
+ * and you may not use the same element twice.
+ *
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ * Created by rns on 2017/3/27.
+ */
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        return null;
+    }
+    public static void main(String [] args){
+        System.out.println();
+    }
+
+}
