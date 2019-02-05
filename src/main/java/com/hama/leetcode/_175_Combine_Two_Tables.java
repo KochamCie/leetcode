a5f21744f7e428d754e16cb09ccc40bf21f687fa
rns <745258662@qq.com>
2019-02-05 23:47:19 +0800

    [leetcode] 175 Combine Two Tables

---
+++ src/main/java/com/hama/leetcode/_175_Combine_Two_Tables.java
@@ -0,0 +1,47 @@
+package com.hama.leetcode;
+
+/**
+ * @Author: rns
+ * @Date: 2019/2/5 下午11:46
+ * @Description: _175_Combine_Two_Tables
+ * Table: Person
+ *
+ * +-------------+---------+
+ * | Column Name | Type    |
+ * +-------------+---------+
+ * | PersonId    | int     |
+ * | FirstName   | varchar |
+ * | LastName    | varchar |
+ * +-------------+---------+
+ * PersonId is the primary key column for this table.
+ * Table: Address
+ *
+ * +-------------+---------+
+ * | Column Name | Type    |
+ * +-------------+---------+
+ * | AddressId   | int     |
+ * | PersonId    | int     |
+ * | City        | varchar |
+ * | State       | varchar |
+ * +-------------+---------+
+ * AddressId is the primary key column for this table.
+ *
+ *
+ * Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
+ *
+ * FirstName, LastName, City, State
+ */
+public class _175_Combine_Two_Tables {
+
+    /**
+     * # Write your MySQL query statement below
+     *
+     * select
+     *     p.FirstName,
+     *     p.LastName,
+     *     a.City,
+     *     a.State
+     * from Person p
+     * left join Address a on p.PersonId=a.PersonId
+     */
+}
